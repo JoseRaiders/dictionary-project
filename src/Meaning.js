dd759e1217d1fc6dbc4db89b8596d37cb0a8d734
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2021-12-04 11:12:57 -0800

    New example component added

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import Example from "./Example";
 import "./Meaning.css";
 
 export default function Meaning(props) {
@@ -13,7 +14,7 @@ export default function Meaning(props) {
                         <p>
                             <strong>Definition:</strong> {definition.definition}
                             <br />
-                            <strong>Example:</strong> <em>{definition.example}</em>
+                            <Example example={definition.example} />
                             <Synonyms synonyms={definition.synonyms} />
                         </p>
                     </div>
