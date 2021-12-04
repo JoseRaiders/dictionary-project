26750501a8bcbe27167b6161f889d9601871e79a
JoseRaiders <71295774+JoseRaiders@users.noreply.github.com>
2021-12-04 12:44:00 -0800

    Styled application

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Phonetic(props) {
     console.log(props.phonetic);
     return (
-        <div class="Phonetic">
+        <div className="Phonetic">
             <h5>{props.phonetic.text}</h5>
             <audio controls src={props.phonetic.audio} type="audio/mpeg"></audio>
         </div>
